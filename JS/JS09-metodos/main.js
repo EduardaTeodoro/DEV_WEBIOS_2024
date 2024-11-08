6a28210d1ecebf400d7de4acd60cfa8f03bf1141
EduardaTeodoro <eduarda.teodoro2004@gmail.com>
2024-11-07 21:30:11 -0300

    js09

---
+++ JS/JS09-metodos/main.js
@@ -0,0 +1,20 @@
+let frutas = ["banana", "morango", "melancia", "uva", "kiwi", "maracuja", "maça", "pera" ]
+
+frutas.forEach((valor, indice) => {
+    console.log(valor);
+    console.log(indice)
+
+})
+
+// frutas.forEach(percorrer)
+// //                conteudo  indice
+// function percorrer(valor, indice){
+//     console.log(valor);
+//     console.log(indice)
+
+// }
+
+// for(i = 0; i <= frutas.length ; i++){
+// console.log(frutas[i]); //valor da posição
+// console.log(i) //numero da posição
+// }
