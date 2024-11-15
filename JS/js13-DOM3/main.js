9d4883b0e655dca9d9edc8a80de0bb22ddc5f756
EduardaTeodoro <eduarda.teodoro2004@gmail.com>
2024-11-14 21:19:39 -0300

    AULA JS13

---
+++ JS/js13-DOM3/main.js
@@ -0,0 +1,15 @@
+let botao = document.querySelector('button')
+
+let olhaNumero = true
+if(olhaNumero === true){
+    botao.style.color = "red"
+    botao.style.backgroundColor = "green"
+    botao.style.borderRadius = "5px"
+    botao.innerHTML = "cadastrar"
+}else{
+    botao.style.color = "red"
+}
+
+let paragrafo = document.querySelector('#paragrafo')
+paragrafo.style.backgroundImage = 'linear-gradient(blue, red)'
+paragrafo.style.color = "white"
